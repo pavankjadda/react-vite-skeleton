1fefe78eebb3a3a7172164f37bfafcc12b293874
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-07-15 18:09:25 -0400

    chore: removes extra margin

--- src/components/home/book-tile/BookTile.tsx
+++ src/components/home/book-tile/BookTile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Card, CardContent, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import {Card, CardContent, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import styles from "../../layout/side-bar/SideBar.module.scss";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import FindInPageIcon from "@mui/icons-material/FindInPage";
 
 export default function BookTile() {
@@ -10,20 +10,20 @@ export default function BookTile() {
     return (
       <div style={{ marginTop: "20px" }}>
           <Card className="container-fluid col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8" elevation={24}>
-              <h3 className="custom-flex-justify-center" style={{ marginTop: "20px" }}>
-                  Book
-              </h3>
-              <Divider style={{ borderBlockEnd: "1px solid black" }} />
-                <CardContent className="row custom-flex-justify-center">
-                    <List>
-                        <ListItem disablePadding>
-                            <ListItemButton onClick={() => history.push("/book")}>
-                                <ListItemIcon className={styles.listItemIcon}>
-                                    {<FindInPageIcon color={"primary"}/>}
-                                </ListItemIcon>
-                                <ListItemText primary="Find Book"/>
-                            </ListItemButton>
-                        </ListItem>
+            <h3 className="custom-flex-justify-center" style={{marginTop: "20px"}}>
+              Book
+            </h3>
+            <Divider/>
+            <CardContent className="row custom-flex-justify-center">
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton onClick={() => history.push("/book")}>
+                    <ListItemIcon className={styles.listItemIcon}>
+                      {<FindInPageIcon color={"primary"}/>}
+                    </ListItemIcon>
+                    <ListItemText primary="Find Book"/>
+                  </ListItemButton>
+                </ListItem>
                         <ListItem disablePadding>
                             <ListItemButton onClick={() => history.push("/book/all")}>
                                 <ListItemIcon className={styles.listItemIcon}>
