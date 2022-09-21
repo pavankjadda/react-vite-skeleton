5ed8c03efbcd27828c49a6d34f3a495bcf803b7e
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-09-20 21:25:20 -0400

    fix(book): fixes Book type

--- src/state/reducers/BookEntityReducer.tsx
+++ src/state/reducers/BookEntityReducer.tsx
@@ -1,5 +1,5 @@
-import {createEntityAdapter, createSlice,} from '@reduxjs/toolkit';
-import {Book} from '../api/bookApi';
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { Book } from '../../types/Book';
 
 /**
  * Define Books Adapter that stores all books entities
@@ -8,10 +8,10 @@ import {Book} from '../api/bookApi';
  * @since 1.0.0
  */
 export const bookManagerAdapter = createEntityAdapter<Book>({
-    // Assume IDs are stored in a field other than `book.id`
-    selectId: (book) => book.id,
-    // Keep the "all IDs" array sorted based on Protocol Number
-    sortComparer: (a, b) => a.isbn.localeCompare(b.isbn),
+	// Assume IDs are stored in a field other than `book.id`
+	selectId: (book) => book.id,
+	// Keep the "all IDs" array sorted based on Protocol Number
+	sortComparer: (a, b) => a.isbn.localeCompare(b.isbn),
 });
 
 /**
@@ -21,14 +21,14 @@ export const bookManagerAdapter = createEntityAdapter<Book>({
  * @since 1.0.0
  */
 export const bookManagerSlice = createSlice({
-    name: 'books',
-    initialState: bookManagerAdapter.getInitialState(),
-    reducers: {
-        // Can pass adapter functions directly as case reducers.  Because we're passing this as a value, `createSlice` will auto-generate the `bookAdded` action type / creator
-        bookAdded: bookManagerAdapter.addOne,
-        bookUpdated: bookManagerAdapter.updateOne,
-        bookAddedOrUpdated: bookManagerAdapter.upsertOne,
-    },
+	name: 'books',
+	initialState: bookManagerAdapter.getInitialState(),
+	reducers: {
+		// Can pass adapter functions directly as case reducers.  Because we're passing this as a value, `createSlice` will auto-generate the `bookAdded` action type / creator
+		bookAdded: bookManagerAdapter.addOne,
+		bookUpdated: bookManagerAdapter.updateOne,
+		bookAddedOrUpdated: bookManagerAdapter.upsertOne,
+	},
 });
 
 /**
@@ -37,8 +37,4 @@ export const bookManagerSlice = createSlice({
  * @author Pavan Kumar Jadda
  * @since 1.0.0
  */
-export const {
-    bookAdded,
-    bookUpdated,
-    bookAddedOrUpdated
-} = bookManagerSlice.actions;
+export const { bookAdded, bookUpdated, bookAddedOrUpdated } = bookManagerSlice.actions;
